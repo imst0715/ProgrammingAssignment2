40eb380bef2213414c94e5972f8c0a98a618ed29
imst0715 <biosupernatant@gmail.com>
2015-04-26 23:01:39 +0800

    finished assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is to caculated the inverse of matrix, to cache the results and to return the cached inverse if the input argument is repeated.
 
-## Write a short comment describing this function
+## This function is to create a list containing 4 functions that assign arguments later used.
+makeCacheMatrix <- function (x=matrix()){
+	i <-NULL
+	set <- function(y){
+		x<<-y
+		i<<-NULL}
+	get <-function()x
+	setinv<-function(inv) i<<-inv
+	getinv<-function()i
+	list(set=set,get=get,setinv=setinv,getinv=getinv)
+	} 
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## This function is to calculate the inverse of matrix 'x' or to return the inverse matrix of which the matrix 'x' already exists.
+cacheSolve<-function(x,...){
+	i<-x$getinv()
+	if(!is.null(i)){
+		message("getting cached data")
+		return(i)}
+	data <-x$get()
+	i<-solve(data,...)
+	x$setinv(i)
+	i
+	}
